872823a9f092de0f8db3197b3f907200a9ea3d9f
1 <ppark9553@gmail.com>
2018-07-11 10:16:23 +0900

    removing await in callback function

--- gobble.js
+++ gobble.js
@@ -229,7 +229,7 @@ app.get('/MASS_DATE_CRAWL', async (req, res) => {
   })
   .catch( error => {
     logger.setCacheLog(taskName, 0, false)
-    await fn.done()
+    fn.done()
     res.status(501) // not implemented error
     res.send(error)
   })
@@ -285,7 +285,7 @@ app.get('/DAILY_CRAWL_ALL', async (req, res) => {
     // pass
   })
   .catch( error => {
-    await fn.done()
+    fn.done()
     res.status(501)
     res.send(error)
   })
