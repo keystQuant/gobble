9abb3519f5ccdeba250ee07987dad8f58e87f462
1 <ppark9553@gmail.com>
2018-07-12 03:44:19 +0900

    tested daily crawl test

--- gobble.js
+++ gobble.js
@@ -1,5 +1,6 @@
 // start js-gobble server on Gobble Server in a Docker container
-//kill $(ps ax | grep '[j]s' | awk '{ print $1 }')     --> reload atom
+// kill $(ps ax | grep '[j]s' | awk '{ print $1 }')     --> reload atom
+// to kill puppeteer process: ps -A | grep <application_name> | awk '{print $1}' | xargs kill -9 $1
 const express = require('express')
 const axios = require('axios')
 
@@ -14,9 +15,13 @@ const {
   TASKSENDER,
 } = require('./src')
 
+const IP = CONFIG.ip.gateway
+const RUN_ENV = 'local' // change to 'local' when testing
+const RUN_HEADLESS = false // change to 'false' when local
+
 const cache = new CACHE.RedisClient()
 const logger = new LOGGER.CacheLogger(cache)
-const taskSender = new TASKSENDER.TaskSender(logger, 'remote') // change to 'local' when testing
+const taskSender = new TASKSENDER.TaskSender(logger, RUN_ENV)
 const api = new API.API()
 
 const app = express()
@@ -70,6 +75,7 @@ const runningTask = async (taskName, res) => {
     if (taskStartKeyValue == 'RUNNING') {
       res.status(501)
       res.send('PROCESS_ALREADY_RUNNING')
+      return 'KILL'
     } else if (taskStartKeyValue == 'STOPPED') {
       await cache.setKey(taskStartKey, 'RUNNING')
     }
@@ -98,9 +104,62 @@ const pollingSignal = async (signal) => {
   return startSignal
 }
 
-const massCrawlTask = async (req, res) => {
-  // 1. define taskName and dataType
+const StartFnBrowser = async (req, res, taskName) => {
+  // start fnguide puppet
+  let fn = new FNGUIDE.Puppet(taskName)
+  await fn.startBrowser(RUN_HEADLESS, 100)
+  .then( response => { console.log(response) })
+  .catch( error => { res.send(error) })
+  // 3. login to fnguide as user
+  await fn.login()
+  .then( response => {
+    // pass
+  })
+  .catch( error => {
+    logger.setCacheLog(taskName, 0, false)
+    fn.done()
+    res.status(501) // not implemented error
+    res.send(error)
+  })
+
+  return fn // returns fn puppet
+}
+
+const massDateCrawlTask = async (req, res, fn='NONE') => {
+  console.log('MASS_DATE_CRAWL task received')
+  // 1. define taskName
   let taskName = req.url.split('/')[1]
+
+  // 2. start fnguide puppet
+  if (fn == 'NONE') {
+    fn = await StartFnBrowser(req, res, taskName)
+  }
+
+  // 3. get mass date API and close browser
+  let dateData = await fn.massDateCrawl()
+
+  // 4. save list data to cache
+  let p = new PROCESSOR.Processor(dateData)
+  let datesData = await p.processMassDate()
+
+  await cache.setList(datesData)
+  // 5. send data task to gateway server (MASS_DATE_SAVE)
+  await taskSender.setCurrentTask(taskName)
+  await taskSender.sendNextTask('MASS_DATE_SAVE')
+
+  // log end process
+  await logger.setCacheLog(taskName, 1, false)
+
+  console.log('MASS_DATE_CRAWL task done')
+
+  return fn // return fnguide browser so process can turn off
+}
+
+const massCrawlTask = async (req, res, fn='NONE', taskName='NONE') => {
+  // 1. define taskName and dataType
+  if (taskName == 'NONE') {
+    taskName = req.url.split('/')[1]
+  }
   // await runningTask(taskName, res)
   let dataType = taskName.split('_')[1]
 
@@ -125,31 +184,16 @@ const massCrawlTask = async (req, res) => {
 
   let dates = await cache.getList(datesListKey)
   if (dates.length == 0) {
+    console.log('NO DATE TO UPDATE')
     console.log(`${taskName} task done`)
-    res.status(200)
-    res.send('NO DATE TO UPDATE')
   }
-
-  // continue if dates exist
-  let status = 'NONE'
 
   // common puppet tasks:
 
   // 1. start fnguide puppet
-  let fn = new FNGUIDE.Puppet(taskName)
-  await fn.startBrowser(true, 100) // set to false when 'local'
-  .then( response => { console.log(response) })
-  .catch( error => { res.send(error) })
-  // 2. login to fnguide as user
-  await fn.login()
-  .then( response => {
-    // pass
-  })
-  .catch( error => {
-    logger.setCacheLog(taskName, 0, true)
-    res.status(501)
-    res.send(error)
-  })
+  if (fn == 'NONE') {
+    fn = await StartFnBrowser(req, res, taskName)
+  }
 
   for (let date of dates) {
 
@@ -195,112 +239,125 @@ const massCrawlTask = async (req, res) => {
     // add date to mass cache lot
     await logger.setMassCacheLog(taskName, date)
 
-    let sleepStatus = await axios.get('http://127.0.0.1:8000/hidden-api/legacy-task/?type=TIME_SLEEP')
+    let sleepURL = ''
+    if (RUN_ENV == 'remote') {
+      sleepURL = `http://${IP}/hidden-api/legacy-task/?type=TIME_SLEEP`
+    } else if (RUN_ENV == 'local') {
+      sleepURL = 'http://127.0.0.1:8000/hidden-api/legacy-task/?type=TIME_SLEEP'
+    }
+    let sleepStatus = await axios.get(sleepURL)
     if (sleepStatus.data.status == 'DONE') {
       console.log('data crawled success: ' + date)
     }
   }
 
-  // close puppet
-  await fn.done()
-
-  status = 'DONE'
   console.log(`${taskName} task done`)
   // await endingTask(taskName)
-  res.status(200)
-  res.send(status)
+
+  return fn // return fnguide browser so process can turn off
 }
 
 
 ////// REAL TASK API'S START HERE //////
 app.get('/MASS_DATE_CRAWL', async (req, res) => {
-  console.log('MASS_DATE_CRAWL task received')
-  // 1. define taskName
-  let taskName = req.url.split('/')[1]
-  // 2. start fnguide puppet
-  let fn = new FNGUIDE.Puppet(taskName)
-  await fn.startBrowser(true, 100) // set to false when 'local'
-  .then( response => { console.log(response) })
-  .catch( error => { res.send(error) })
-  // 3. login to fnguide as user
-  await fn.login()
-  .then( response => {
-    // pass
-  })
-  .catch( error => {
-    logger.setCacheLog(taskName, 0, false)
-    fn.done()
-    res.status(501) // not implemented error
-    res.send(error)
-  })
-  // 4. get mass date API and close browser
-  let dateData = await fn.massDateCrawl()
-  await fn.done()
-  // 5. save list data to cache
-  let p = new PROCESSOR.Processor(dateData)
-  let datesData = await p.processMassDate()
-
-  await cache.setList(datesData)
-  // 6. send data task to gateway server (MASS_DATE_SAVE)
-  await taskSender.setCurrentTask(taskName)
-  await taskSender.sendNextTask('MASS_DATE_SAVE')
-
-  // log end process
-  await logger.setCacheLog(taskName, 1, false)
-
-  console.log('MASS_DATE_CRAWL task done')
+  let fnBrowser = await massDateCrawlTask(req, res, 'NONE')
+  await fnBrowser.done()
   res.status(200)
   res.send('DONE')
 })
 
 app.get('/MASS_INDEX_CRAWL', async (req, res) => {
-  massCrawlTask(req, req)
+  let fnBrowser = await massCrawlTask(req, res, 'NONE', 'NONE')
+  await fnBrowser.done()
+  res.status(200)
+  res.send('DONE')
 })
 
 app.get('/MASS_OHLCV_CRAWL', async (req, res) => {
-  massCrawlTask(req, req)
+  let fn = await massCrawlTask(req, res, 'NONE', 'NONE')
+  await fn.done()
+  res.status(200)
+  res.send('DONE')
 })
 
 app.get('/MASS_BUYSELL_CRAWL', async (req, res) => {
-  massCrawlTask(req, req)
+  let fn = await massCrawlTask(req, res, 'NONE', 'NONE')
+  await fn.done()
+  res.status(200)
+  res.send('DONE')
 })
 
 app.get('/MASS_FACTOR_CRAWL', async (req, res) => {
-  massCrawlTask(req, req)
+  let fn = await massCrawlTask(req, res, 'NONE', 'NONE')
+  await fn.done()
+  res.status(200)
+  res.send('DONE')
 })
 
 // crawling all data once (daily, every 30 minutes if date updated)
 app.get('/DAILY_CRAWL_ALL', async (req, res) => {
+  let taskLog = ''
+
   let taskName = req.url.split('/')[1]
-  // await runningTask(taskName, res)
+  await runningTask(taskName, res)
 
-  // 1. start fnguide puppet
-  let fn = new FNGUIDE.Puppet('MASS_INDEX_CRAWL')
-  await fn.startBrowser(true, 100) // set to false when 'local'
-  .then( response => { console.log(response) })
-  .catch( error => { res.send(error) })
-  // 2. login to fnguide as user
-  await fn.login()
-  .then( response => {
-    // pass
-  })
-  .catch( error => {
-    fn.done()
-    res.status(501)
-    res.send(error)
-  })
+  // 1. MASS_DATE_CRAWL (Node)
+  let fn = await massDateCrawlTask(req, res, 'NONE')
+  taskLog = 'MASS_DATE_CRAWL'
 
-  // get mass index API and close browser
-  let crawledData = await fn.massIndexCrawl('20180705')
-  // save list data to cache
-  let p = new PROCESSOR.Processor(crawledData)
-  let processedData = await p.processMassIndex('20180705')
+  let keystDates = await cache.getList('keyst_dates')
+  let mostCurrentDate = keystDates[0]
+  taskLog = taskLog + `(${mostCurrentDate})`
 
-  let testData = processedData[1]
+  // 2. check most recent db updates
+  let updatesURL = ''
+  if (RUN_ENV == 'remote') {
+    updatesURL = `http://${IP}/hidden-api/legacy-task/?type=SET_RECENT_UPDATE_DATES`
+  } else if (RUN_ENV == 'local') {
+    updatesURL = 'http://127.0.0.1:8000/hidden-api/legacy-task/?type=SET_RECENT_UPDATE_DATES'
+  }
+  let setState = await axios.get(updatesURL)
+
+  let tasksList = []
+  if (setState.data.status == 'DONE') {
+    let recentIndexDate = await cache.getKey('RECENT_INDEX_DATE')
+    let recentOhlcvDate = await cache.getKey('RECENT_OHLCV_DATE')
+    let recentBuysellDate = await cache.getKey('RECENT_BUYSELL_DATE')
+    let recentFactorDate = await cache.getKey('RECENT_FACTOR_DATE')
+    console.log('Recent Index data update date: ' + recentIndexDate)
+    console.log('Recent OHLCV data update date: ' + recentOhlcvDate)
+    console.log('Recent BuySell data update date: ' + recentBuysellDate)
+    console.log('Recent Factor data update date: ' + recentFactorDate)
+    taskLog = taskLog + ` Index: ${recentIndexDate} OHLCV: ${recentOhlcvDate} BuySell: ${recentBuysellDate} Factor: ${recentFactorDate}`
+
+    if (recentIndexDate != mostCurrentDate) {
+      tasksList.push('MASS_INDEX_CRAWL')
+    }
+
+    if (recentOhlcvDate != mostCurrentDate) {
+      tasksList.push('MASS_OHLCV_CRAWL')
+    }
+
+    if (recentBuysellDate != mostCurrentDate) {
+      tasksList.push('MASS_BUYSELL_CRAWL')
+    }
+
+    if (recentFactorDate != mostCurrentDate) {
+      tasksList.push('MASS_FACTOR_CRAWL')
+    }
+  }
+
+  if (tasksList.length != 0) {
+    // only run task loop when there are data needed for update
+    for (let task of tasksList) {
+      fn = await massCrawlTask(req, res, fn, task)
+      taskLog = taskLog + ` ${task}`
+    }
+  }
 
   await fn.done()
-
-  await api.saveState(taskName, testData)
+  await endingTask(taskName)
+  await api.saveState(taskName, taskLog)
   // await endingTask(taskName)
   res.status(200)
   res.send('DONE')
